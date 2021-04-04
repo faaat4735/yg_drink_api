d2e6724a07d12ee0fc100392869a21f04f848c43
faaat <zjf580@163.com>
2021-04-04 11:02:30 +0800

    添加喝水接口

--- app/controllers/ActionController.php
+++ app/controllers/ActionController.php
@@ -10,7 +10,7 @@ class ActionController extends Controller
         $drinkOnce = 200;
         $sql = 'INSERT INTO t_user_drink SET user_id = ?, drink_type = ?, create_date = ?, drink_quantity = ?';
         $this->db->exec($sql, $this->userId, 1, date('Y-m-d'), $drinkOnce);
-        $sql = 'SELECE total_id, drink_total, drink_target, is_reach FROM t_user_drink_total WHERE user_id = ? AND create_date = ?';
+        $sql = 'SELECT total_id, drink_total, drink_target, is_reach FROM t_user_drink_total WHERE user_id = ? AND create_date = ?';
         $totalInfo = $this->db->getRow($sql, $this->userId, date('Y-m-d'));
         if ($totalInfo) {
             $sql = 'UPDATE t_user_drink_total SET drink_total = ?';
