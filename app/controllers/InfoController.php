0f54e39ffd65c93fa4567cb27f7db3a45883498a
faaat <zjf580@163.com>
2021-04-04 09:31:39 +0800

    添加喝水接口

--- app/controllers/InfoController.php
+++ app/controllers/InfoController.php
@@ -12,7 +12,7 @@ class InfoController extends Controller
      */
     public function homeAction () {
         $sql = 'SELECT drink_target FROM t_user WHERE user_id = ?';
-        $target = $this->db->getOne($sql, $this->user_id);
+        $target = $this->db->getOne($sql, $this->userId);
         $sql = 'SELECT drink_quantity quantity, UNIX_TIMESTAMP(create_time) * 1000 time, drink_type_name drinkType FROM t_user_drink LEFT JOIN t_drink_type USING(drink_type) WHERE user_id = ? AND create_date = ?';
         $list = $this->db->getAll($sql, $this->userId, date("Y-m-d"));
         return array('current' => array_sum(array_column($list, 'quantity')), 'target' => $target, 'list' => $list);
