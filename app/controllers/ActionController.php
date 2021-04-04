037ab9171c9e49c16c5627f7dcff0a98890c974a
faaat <zjf580@163.com>
2021-04-04 09:25:34 +0800

    添加喝水接口

--- app/controllers/ActionController.php
+++ app/controllers/ActionController.php
@@ -7,6 +7,8 @@ use Core\Controller;
 class ActionController extends Controller
 {
     public function drink () {
+        $sql = 'INSERT INTO t_user_drink SET user_id = ?, drink_type = ?, create_date = ?, quantity = ?';
+        $this->db->exec($sql, $this->userId, 1, date('Y-m-d'), 200);
         return array();
     }
 }
--- app/controllers/InfoController.php
+++ app/controllers/InfoController.php
@@ -11,6 +11,11 @@ class InfoController extends Controller
      * @return array
      */
     public function homeAction () {
-        return array('current' => 1200, 'target' => 2400, 'list' => array(array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200)));
+        $sql = 'SELECT drink_target FROM t_user WHERE user_id = ?';
+        $target = $this->db->getOne($sql, $this->user_id);
+        $sql = 'SELECT drink_quantity quantity, UNIX_TIMESTAMP(create_time) * 1000 time, drink_type_name drinkType FROM t_user_drink LEFT JOIN t_drink_type USING(drink_type) WHERE user_id = ? AND create_date = ?';
+        $list = $this->db->getAll($sql, $this->userId, date("Y-m-d"));
+        return array('current' => array_sum(array_column($list, 'quantity')), 'target' => $target, 'list' => $list);
+//        return array('current' => 1200, 'target' => 2400, 'list' => array(array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200)));
     }
 }
