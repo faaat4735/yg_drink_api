d1dbf3e3afe38e587470b94f9875f37615145902
faaat <zjf580@163.com>
2021-05-09 12:46:58 +0800

    倒叙喝水记录

--- app/controllers/InfoController.php
+++ app/controllers/InfoController.php
@@ -13,7 +13,7 @@ class InfoController extends Controller
     public function homeAction () {
         $sql = 'SELECT drink_target FROM t_user WHERE user_id = ?';
         $target = $this->db->getOne($sql, $this->userId);
-        $sql = 'SELECT drink_quantity quantity, UNIX_TIMESTAMP(create_time) * 1000 time, drink_type_name drinkType FROM t_user_drink LEFT JOIN t_drink_type USING(drink_type) WHERE user_id = ? AND create_date = ?';
+        $sql = 'SELECT drink_quantity quantity, UNIX_TIMESTAMP(create_time) * 1000 time, drink_type_name drinkType FROM t_user_drink LEFT JOIN t_drink_type USING(drink_type) WHERE user_id = ? AND create_date = ? ORDER BY drink_id DESC';
         $list = $this->db->getAll($sql, $this->userId, date("Y-m-d"));
         return array('current' => array_sum(array_column($list, 'quantity')), 'target' => $target, 'list' => $list);
 //        return array('current' => 1200, 'target' => 2400, 'list' => array(array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200), array('time' => time() * 1000, 'drinkType' => '水', 'quantity' => 200)));
