beb55349245cf988caa32abfcd177e5b5f1c307b
faaat <zjf580@163.com>
2021-05-17 08:51:06 +0800

    测试历史列表

--- app/controllers/InfoController.php
+++ app/controllers/InfoController.php
@@ -26,9 +26,7 @@ class InfoController extends Controller
         $reachCount = $this->db->getOne($sql, $this->userId, 1);
         $sql = 'SELECT create_date, drink_total FROM t_user_drink_total WHERE user_id = ? AND create_date <= ? AND create_date >= ?';
         $listInfo = $this->db->getPairs($sql, $this->userId, date('Y-m-d', strtotime('-1 day')), date('Y-m-d', strtotime('-7 day')));
-        var_dump($listInfo);
         $dateList = array(date('Y-m-d', strtotime('-7 day')), date('Y-m-d', strtotime('-6 day')), date('Y-m-d', strtotime('-5 day')), date('Y-m-d', strtotime('-4 day')), date('Y-m-d', strtotime('-3 day')), date('Y-m-d', strtotime('-2 day')), date('Y-m-d', strtotime('-1 day')));
-        var_dump($dateList);
 
         foreach ($dateList as $date) {
             if (isset($listInfo[$date])) {
