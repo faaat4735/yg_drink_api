f97f40a3b1ff23e7ebec68322cdba7e76a4a9bfa
faaat <zjf580@163.com>
2021-09-14 10:42:34 +0800

    任务信息接口

--- app/core/Task.php
+++ app/core/Task.php
@@ -70,22 +70,90 @@ class Task extends Controller
     }
 
     protected function _getInfo() {
-        $activityInfo = $this->model->activity->info($this->type);
-        $activityReceiveInfo = $this->model->gold->receiveGold($this->userId, $this->type);
-        $return = array();
-        if ($activityInfo['activity_max'] > $activityReceiveInfo['count']) {
-            $sql = 'SELECT award_min, award_max FROM t_award_config WHERE config_type = ? AND counter <= ? ORDER BY counter DESC LIMIT 1';
-            $awardRange = $this->db->getRow($sql, $this->type, $activityReceiveInfo['count'] + 1);
-            $return = array('count' => $activityReceiveInfo['count'] + 1, 'num' => rand($awardRange['award_min'], $awardRange['award_max']),'type' => $this->type, 'receiveTime' => (($activityReceiveInfo['maxTime'] ? strtotime($activityReceiveInfo['maxTime']) + $activityInfo['activity_duration'] * 60 : time())) * 1000);
+        switch ($this->type) {
+            case 'drink':
+                $return['type'] = 'interior';
+                $return['url'] = 'home';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "drink");
+                $return['name'] = '完成首次喝水打卡';
+                $return['status'] = 0;
+                break;
+            case 'drink_total':
+                $return['type'] = 'interior';
+                $return['url'] = 'home';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "drink_total");
+                $return['name'] = '累计完成4次喝水(0/4)';
+                $return['status'] = 0;
+                break;
+            case 'drink_target':
+                $return['type'] = 'interior';
+                $return['url'] = 'home';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "drink_target");
+                $return['name'] = '完成今日喝水目标';
+                $return['status'] = 0;
+                break;
+            case 'video':
+                $return['type'] = 'popup';
+                $return['url'] = 'video';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "video");
+                $return['name'] = '看创意视频(0/3)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
+            case 'wechat':
+                $return['type'] = 'popup';
+                $return['url'] = 'wechat';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "wechat");
+                $return['name'] = '绑定微信号';
+                $return['status'] = 0;
+                break;
+            case 'sport_ywzz':
+                $return['type'] = 'popup';
+                $return['url'] = 'sport_ywzz';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "sport_ywzz");
+                $return['name'] = '仰卧起坐(0/20)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
+            case 'sport_zyz':
+                $return['type'] = 'popup';
+                $return['url'] = 'sport_zyz';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "sport_zyz");
+                $return['name'] = '走一走(0/10)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
+            case 'sport_pyp':
+                $return['type'] = 'popup';
+                $return['url'] = 'sport_pyp';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "sport_pyp");
+                $return['name'] = '跑一跑(0/8)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
+            case 'sport_yy':
+                $return['type'] = 'popup';
+                $return['url'] = 'sport_yy';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "sport_yy");
+                $return['name'] = '游泳(0/4)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
+            case 'sport_hwyy':
+                $return['type'] = 'popup';
+                $return['url'] = 'sport_hwyy';
+                $return['goldInfo'] = array('count' => 1, 'num' => 20,'type' => "sport_hwyy");
+                $return['name'] = '户外运动(0/2)';
+                $return['receiveTime'] = time() * 1000;
+                $return['status'] = 0;
+                break;
         }
-        $return['maxReceive'] = $activityInfo['activity_max'];
-        $return['currentReceive'] = $activityReceiveInfo['count'];
         $return['serverTime'] = time() * 1000;
         return $return;
     }
 
     protected function _receiveAward ($data) {
-        $this->model->gold->insert(array('user_id' => $this->userId, 'gold_count' => $data['count'], 'gold_amount' => $data['num'], 'gold_source' => $data['type'], 'isDouble' => $data['isDouble'] ?? 0));
+        $this->model->gold->insert(array('user_id' => $this->userId, 'gold_count' => $data['count'], 'gold_amount' => $data['num'], 'gold_source' => $data['type'], 'isDouble' => $data['isDouble'] ?? 0, 'isFive' => $data['isFive'] ?? 0));
         if (in_array($data['type'], array('drink', 'walk', 'walk_stage', 'newer'))) {
             return array();
         }
